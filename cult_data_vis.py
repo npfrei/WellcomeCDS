380db812c28ab3abe9383593b573634052eed4b6
npfrei <126064815+npfrei@users.noreply.github.com>
2025-03-20 11:36:42 +0100

    visualization stuff (to clean for presentation)

--- cult_data_vis.py
+++ cult_data_vis.py
@@ -5,8 +5,31 @@ import matplotlib.pylab as plt
 import csv
 url = "https://api.wellcomecollection.org/catalogue/v2/images"
 from collections import Counter
+from collections import OrderedDict
 
 results = {}
+
+def plt_csv(file ,xlabel,ylabel, ytype: type, xtype:type, min, plotf):
+    xy = OrderedDict()
+    x = []
+    y = []
+    with open(file,'r') as csvfile: 
+        plots = csv.reader(csvfile, delimiter = ',') 
+        
+        for row in plots: 
+        
+            if not not row and ytype(row[1]) > min:
+                xy[ytype(row[1])] =  xtype(row[0])
+    for key, value in xy.items() :
+        x.append(value)
+        y.append(key)
+    plotf(  x,y, color = 'g', label = ylabel) 
+
+    plt.xlabel(xlabel) 
+    plt.ylabel(ylabel) 
+    plt.xticks(rotation=70)
+    plt.legend() 
+    plt.show() 
 
 # count for every year (1500-2021)
 """
@@ -61,10 +84,13 @@ with open('genres.csv', 'w') as csv_file:
     for key, value in final_count.items():
        writer.writerow([key, value])
 print(final_count)
-""" 
+
 
 response = requests.get(
         url,
         params={"aggregations" :  "locations.license", "pageSize" : "100"}).json()
 
 print(response["results"])
+""" 
+plt_csv('years.csv', "Year", "Number of Images", int, int, 0, plt.bar)
+plt_csv("genres.csv", "Genre", "Number of Images", int, str, 5, plt.barh)
