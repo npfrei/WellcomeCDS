64d95cce7ce3839e9d1136a053bea3ffe5ec34b8
npfrei <126064815+npfrei@users.noreply.github.com>
2025-03-22 11:43:37 +0100

    add helpers file

--- cult_data_vis.py
+++ cult_data_vis.py
@@ -11,32 +11,9 @@ import os
 import numpy as np
 from itertools import chain
 import seaborn as sns
-
+import helpers
 results = {}
 
-def plt_csv(file ,xlabel,ylabel, ytype: type, xtype:type, min, plotf):
-    xy = OrderedDict()
-    x = []
-    y = []
-    with open(file,'r') as csvfile: 
-        plots = csv.reader(csvfile, delimiter = ',') 
-        
-        for row in plots: 
-        
-            if not not row and ytype(row[1]) > min:
-                xy[ytype(row[1])] =  xtype(row[0])
-    for key, value in xy.items() :
-        x.append(value)
-        y.append(key)
-    plotf(  x,y, color = 'g', label = ylabel) 
-
-    plt.xlabel(xlabel) 
-    plt.ylabel(ylabel) 
-    plt.xticks(rotation=70)
-    plt.legend() 
-    plt.show() 
-    
-   
 
 # count for every year (1500-2021)
 """
@@ -65,8 +42,7 @@ with open('years.csv', 'w') as csv_file:
 response = requests.get(
         url,
         params={"include" :  "source.genres", "pageSize" : "100"}
-        
-            
+                   
             
 ).json()
 
@@ -126,10 +102,6 @@ res_count = Counter(resolutions.values())
 
 
 
-with open('res_120000.csv', 'w') as csv_file:  
-    writer = csv.writer(csv_file)
-    for key, value in resolutions.items():
-       writer.writerow([key, value])
 with open('res_count_120000.csv', 'w') as csv_file:  
     writer = csv.writer(csv_file)
     for key, value in res_count.items():
